425d6cd1cd6f585ad4bd3741c6bd3264f07b4c3d
Hanyel Chamon <hanyel.chamon@gmail.com>
2024-06-08 17:54:56 -0300

    adiciona layer com teclas extras

--- keyboards/lily58/keymaps/hanyelC/config.h
+++ keyboards/lily58/keymaps/hanyelC/config.h
@@ -29,6 +29,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 100
 
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
